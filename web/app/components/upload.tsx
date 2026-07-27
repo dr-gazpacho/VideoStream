80365f26a5dd6b4b517badc9820cf8a83b27b68a
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2026-07-26 20:08:43 -0500

    disgusting little uploader

--- web/app/components/upload.tsx
+++ web/app/components/upload.tsx
@@ -1,5 +1,26 @@
 import { Input, ALL_FORMATS, BlobSource } from "mediabunny";
+import { useState } from "react";
 
 export const Upload: React.FC = () => {
-  return <div>The Uploader</div>;
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [input, setInput] = useState<Input<BlobSource> | null>(null);
+
+  function handleUploadFile(event: React.ChangeEvent<HTMLInputElement>): void {
+    const uploadedFile = event.target.files[0];
+    if (uploadedFile.type.startsWith("video")) {
+      const newInput = new Input({
+        formats: ALL_FORMATS,
+        source: new BlobSource(uploadedFile),
+      });
+      setFile(uploadedFile);
+      setInput(newInput);
+    }
+  }
+
+  return (
+    <div>
+      <p>The Uploader</p>
+      <input type="file" id="input" onChange={handleUploadFile} />
+    </div>
+  );
 };
