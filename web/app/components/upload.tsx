4fd593d306d1db0a853ddd77fd5211db5cba69a7
dr-gazpacho <mark.koerner@trms.com>
2026-07-28 17:07:34 -0500

    handle null

--- web/app/components/upload.tsx
+++ web/app/components/upload.tsx
@@ -6,8 +6,8 @@ export const Upload: React.FC = () => {
   const [input, setInput] = useState<Input<BlobSource> | null>(null);
 
   function handleUploadFile(event: React.ChangeEvent<HTMLInputElement>): void {
-    const uploadedFile = event.target.files[0];
-    if (uploadedFile.type.startsWith("video")) {
+    const uploadedFile = !!event?.target?.files && event.target.files[0];
+    if (uploadedFile && uploadedFile.type.startsWith("video")) {
       const newInput = new Input({
         formats: ALL_FORMATS,
         source: new BlobSource(uploadedFile),
@@ -16,6 +16,8 @@ export const Upload: React.FC = () => {
       setInput(newInput);
     }
   }
+
+  console.log(input)
 
   return (
     <div>
